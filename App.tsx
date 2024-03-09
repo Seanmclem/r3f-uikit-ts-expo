c9ebb67bcf22280c339428a2d30fc7fa1d5d489c
Seanmclem <seanmclements@gmail.com>
2024-03-09 17:44:06 -0500

    basic demo borked

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Fullscreen, Container } from "@react-three/uikit";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas>
+      <Fullscreen flexDirection="row" padding={100} gap={100}>
+        <Container
+          flexGrow={1}
+          backgroundOpacity={0.5}
+          hover={{ backgroundOpacity: 1 }}
+          backgroundColor="red"
+        />
+        <Container
+          flexGrow={1}
+          backgroundOpacity={0.5}
+          hover={{ backgroundOpacity: 1 }}
+          backgroundColor="blue"
+        />
+      </Fullscreen>
+    </Canvas>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
